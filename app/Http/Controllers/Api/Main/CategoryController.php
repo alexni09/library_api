815878f23dd08b4f1467123fb8d3fe2688d98344
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-08-29 20:50:33 -0300

    delete a category

--- app/Http/Controllers/Api/Main/CategoryController.php
+++ app/Http/Controllers/Api/Main/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\Main;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-//use Illuminate\Http\Response;
+use Illuminate\Http\Response;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -27,5 +27,10 @@ class CategoryController extends Controller {
     public function update(StoreCategoryRequest $request, Category $category):CategoryResource {
         $category->update($request->validated());
         return CategoryResource::make($category);
+    }
+
+    public function destroy(Category $category):Response {
+        $category->delete();
+        return response()->noContent();
     }
 }
