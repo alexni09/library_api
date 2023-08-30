2891a01ec51c55b490be6e0a8828e49d1eb644cf
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-08-29 22:51:05 -0300

    Update BookFactory.php
    added a better rating

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -16,6 +16,16 @@ class BookFactory extends Factory {
      */
     protected $model = Book::class;
 
+    protected function rating(): int {
+        $r = rand(1,15);
+        switch($r) {
+            case 1: return 1; break;
+            case 2: case 3: return 2; break;
+            case 4: case 5: case 6: return 3; break;
+            case 7: case 8: case 9: case 10: return 4; break;
+            default: return 5; break;
+        }
+    }
     /**
      * Define the model's default state.
      *
@@ -24,7 +34,7 @@ class BookFactory extends Factory {
     public function definition(): array {
         return [
             'name' => $this->faker->text(30),
-            'rating' => rand(1,5)
+            'rating' => $this->rating()
         ];
     }
 }
