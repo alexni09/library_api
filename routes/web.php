4f8b9358b3dd34a33d08716b90bd487396af7fe8
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-03 21:11:43 -0300

    redis tests... (2)

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,17 @@ use Illuminate\Support\Facades\Redis;
 
 
 Route::get('/redis', function () {
-    Redis::set('key2','pigeon');  // a test
-    dd(Redis::get('key2'));
+    //Redis::set('key2','pigeon');   // a test
+    //dd(Redis::get('key2'));
+    Redis::lpush('mylist','111111');
+    Redis::lpush('mylist','22222');
+    Redis::lpush('mylist','333');
+    Redis::lpush('mylist','44444444');
+    Redis::lpush('mylist','5555');
+    //$leng = Redis::llen('mylist');
+    //dd($leng);   // a test
+    $arr = Redis::lrange('mylist',0,-1);
+    dd($arr);   // a test
 });
 
 
