5d43eb41fbd3d088597653fa454bd3e3c2291bb3
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-04 17:24:53 -0300

    added Redis to books insert

--- app/Http/Controllers/Api/Main/BookController.php
+++ app/Http/Controllers/Api/Main/BookController.php
@@ -11,21 +11,34 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Http\Requests\StoreBookRequest;
 use App\Http\Requests\UpdateBookRequest;
 use App\Services\Misc;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
 
 class BookController extends Controller {
     public function index():AnonymousResourceCollection {
-        Misc::monitor('get',200);
+        Misc::monitor('get',Response::HTTP_OK);
         return BookResource::collection(Book::all());
     }
 
     public function show(Book $book):BookResource {
-        Misc::monitor('get',200);
+        Misc::monitor('get',Response::HTTP_OK);
         return BookResource::make($book);
     }
 
-    public function store(StoreBookRequest $request):BookResource {
-        $book = Book::create($request->validated());
-        Misc::monitor('post',201);
+    public function store(Request $request):BookResource|JsonResponse {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'rating' => ['required', 'integer', 'min:1', 'max:5'],
+            'category_id' => ['required', 'integer', 'min:1', 'exists:categories,id']
+        ]);
+        if ($validator->fails()) {
+            Misc::monitor('post',Response::HTTP_UNPROCESSABLE_ENTITY);
+            return response()->json([
+                'errors' => $validator->errors()
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $book = Book::create($validator->validated());
+        Misc::monitor('post',Response::HTTP_CREATED);
         return BookResource::make($book);
     }
 
@@ -37,13 +50,13 @@ class BookController extends Controller {
         if (isset($rating)) $book->rating = $rating;
         if (isset($category_id)) $book->category_id = $category_id;
         $book->save();
-        Misc::monitor('put',200);
+        Misc::monitor('put',Response::HTTP_OK);
         return BookResource::make($book);
     }
 
     public function destroy(Book $book):Response {
         $book->delete();
-        Misc::monitor('delete',204);
+        Misc::monitor('delete',Response::HTTP_NO_CONTENT);
         return response()->noContent();
     }
 }
--- app/Services/Misc.php
+++ app/Services/Misc.php
@@ -6,10 +6,20 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Str;
 
 class Misc {
+    const MAX_MONITORED_LINES = 25;
+
     public static function monitor(string $method, int $status):void {
+        $l = intval(Redis::llen('list_url'));
+        Redis::multi();
         Redis::lpush('list_method',Str::upper($method));
         Redis::lpush('list_url',request()->fullUrl());
         Redis::lpush('list_status',strval($status));
+        if ($l > self::MAX_MONITORED_LINES) {
+            Redis::rpop('list_method', $l - self::MAX_MONITORED_LINES + 1);
+            Redis::rpop('list_url', $l - self::MAX_MONITORED_LINES + 1);
+            Redis::rpop('list_status', $l - self::MAX_MONITORED_LINES + 1);
+        }
+        Redis::exec();
     } 
 
     public static function rating():int {
