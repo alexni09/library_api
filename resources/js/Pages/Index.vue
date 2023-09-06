32175dad7aa08d1583a7df6ddad55cdcb7482083
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-06 08:46:04 -0300

    styles

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -26,19 +26,27 @@ onBeforeUnmount(() => clearInterval(myInterval))
         <table class="bg-zinc-50 border border-zinc-500">
             <tr>
                 <th class="p-1 bg-zinc-300 font-semibold border-b border-zinc-500 whitespace-nowrap">When ?</th>
-                <th class="p-1 bg-zinc-300 font-semibold border-l border-b border-zinc-500">Method</th>
-                <th class="p-1 bg-zinc-300 font-semibold border-l border-b border-zinc-500">URL</th>
-                <th class="p-1 bg-zinc-300 font-semibold border-l border-b border-zinc-500">Status</th>
+                <th class="thStyle">Method</th>
+                <th class="thStyle">URL</th>
+                <th class="thStyle">Status</th>
             </tr>
             <tr v-if="lines?.length === 0">
                 <td colspan="4" class="p-1 font-medium whitespace-nowrap"><i>No Records Found!</i></td>
             </tr>
-            <tr v-for="line in lines" :key="line.id">
-                <td class="px-1 whitespace-nowrap" :class="{ 'bg-zinc-200': line.id % 6 > 2 }">{{ dayjs(line.datetime + '+00:00').format('DD/MM/YYYY HH:mm:ss') }}</td>
-                <td class="px-1 border-l border-zinc-500" :class="{ 'bg-zinc-200': line.id % 6 > 2 }">{{ line.method }}</td>
-                <td class="px-1 border-l border-zinc-500" :class="{ 'bg-zinc-200': line.id % 6 > 2 }">{{ line.url }}</td>
-                <td class="px-1 border-l border-zinc-500" :class="{ 'bg-zinc-200': line.id % 6 > 2 }">{{ line.status }}</td>
+            <tr v-for="line in lines" :key="line.id" :class="{ 'bg-zinc-200': line.id % 6 > 2 }">
+                <td class="px-1 whitespace-nowrap">{{ dayjs(line.datetime + '+00:00').format('DD/MM/YYYY HH:mm:ss') }}</td>
+                <td class="tdStyle">{{ line.method }}</td>
+                <td class="tdStyle">{{ line.url }}</td>
+                <td class="tdStyle">{{ line.status }}</td>
             </tr>
         </table>
     </div>
 </template>
+<style lang="postcss" scoped>
+.tdStyle {
+    @apply px-1 border-l border-zinc-500;
+}
+.thStyle {
+    @apply p-1 bg-zinc-300 font-semibold border-l border-b border-zinc-500;
+}
+</style>
