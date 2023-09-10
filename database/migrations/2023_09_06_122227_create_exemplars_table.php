22fbb06eed8c8a0969cc6953c8a2a3ebc397f601
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-10 15:03:43 -0300

    fee and fines

--- database/migrations/2023_09_06_122227_create_exemplars_table.php
+++ database/migrations/2023_09_06_122227_create_exemplars_table.php
@@ -15,6 +15,12 @@ return new class extends Migration {
             $table->unsignedTinyInteger('condition')->default(1);
             $table->foreignId('book_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('user_id')->nullable()->setNullOnDelete()->cascadeOnUpdate()->default(null);
+            $table->unsignedMediumInteger('maximum_minutes')->default(60);
+            $table->unsignedMediumInteger('fee')->default(6000);               /* one shot */
+            $table->unsignedMediumInteger('fine_per_delay')->default(2200);    /* one shot */
+            $table->unsignedMediumInteger('fine_per_minute')->default(550);
+            $table->unsignedMediumInteger('fine_per_loss')->default(260000);   /* one shot */
+            $table->unsignedMediumInteger('fine_per_damage')->default(39000);  /* one shot */
             $table->timestamps();
             $table->unique(['borrowable', 'id']);
             $table->unique(['condition', 'borrowable', 'id']);
