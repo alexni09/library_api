01bb6c6ca1298b21f5c2c289d5cca94310af2849
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-10 13:45:40 -0300

    borrowed date

--- app/Models/Exemplar.php
+++ app/Models/Exemplar.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Enum\ExemplarCondition;
+use Illuminate\Support\Facades\DB;
 
 class Exemplar extends Model {
     use HasFactory;
@@ -20,4 +21,11 @@ class Exemplar extends Model {
     public function donor() { return $this->belongsTo(User::class); }
     public function borrowed() { return $this->belongsToMany(User::class)->withPivot('borrowed', 'returned'); }
     public function unreturned() { return $this->belongsToMany(User::class)->as('unreturned')->wherePivotNull('returned'); }
+
+    /* Misc */
+    public function borrowedDate() {
+        $e = DB::table('exemplar_user')->where('exemplar_id',$this->id)->whereNull('returned')->get();
+        if ($e->isEmpty()) return null;
+        else return $e[0]->borrowed;
+    }
 }
