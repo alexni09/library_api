2878e19c6b1a0624e0b3cf914c26b41fdca50980
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-15 17:23:52 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,3 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Index');
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
