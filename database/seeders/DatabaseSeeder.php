e01db36dcde034495703aeab8f26df047d0b8c31
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-15 16:22:47 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder {
 
     public function run(): void {
         $this->call(UserSeeder::class);
-        //$this->call(CategorySeeder::class);
-        //$this->call(ExemplarSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ExemplarSeeder::class);
     }
 }
