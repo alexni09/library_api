49c6fbc1e1f03daee34767316fa7c65970ba7ba1
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-15 19:56:21 -0300

    Update CountController.php

--- app/Http/Controllers/Api/Main/CountController.php
+++ app/Http/Controllers/Api/Main/CountController.php
@@ -11,10 +11,14 @@ use Illuminate\Http\JsonResponse;
 
 class CountController extends Controller {
     public function __invoke(Request $request):JsonResponse {
+        $cac = DB::table('categories')->selectRaw('count(distinct id) as qty')->get()[0]->qty;
+        $boc = DB::table('books')->selectRaw('count(distinct id) as qty')->get()[0]->qty;
         $exc = DB::table('exemplars')->selectRaw('count(distinct id) as qty')->get()[0]->qty;
         Misc::monitor('get',Response::HTTP_OK);
         return response()->json([
             'data' => [
+                'category_count' => $cac,
+                'book_count' => $boc,
                 'exemplar_count' => $exc
             ]
         ], Response::HTTP_OK);
