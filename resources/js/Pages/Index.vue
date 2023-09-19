eeea223b76ae9f20ea19030985f37714dead02cc
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-19 16:54:35 -0300

    style changes

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -53,49 +53,51 @@ onBeforeUnmount(() => {
 })
 </script>
 <template>
-    <div class="flex justify-center">
-        <h1 class="mt-2 mb-2 font-bold text-4xl">:<span class="ml-1">:</span> library_api <span class="mr-1">:</span>:</h1>
-    </div>
-    <div class="flex justify-center">
-        <h3 class="mb-4 font-medium text-2xl">Accumulated: <i class="mr-0.5">F</i>{{ accumulatedMoney }}</h3>
-    </div>
-    <div class="flex justify-center">
-        <table class="bg-zinc-50 border border-zinc-500">
-            <tr>
-                <th class="p-1 bg-zinc-300 font-semibold border-b border-zinc-500 whitespace-nowrap">When ?</th>
-                <th class="thStyle">Method</th>
-                <th class="thStyle">URL</th>
-                <th class="thStyle">IP</th>
-                <th class="thStyle">Status</th>
-            </tr>
-            <tr v-if="lines?.length === 0">
-                <td colspan="5" class="p-1 font-medium italic whitespace-nowrap">No Records Found!</td>
-            </tr>
-            <tr v-for="line in lines" :key="line.id" :class="{ 'bg-zinc-200': line.id % 6 > 2 }">
-                <td class="px-1 whitespace-nowrap">{{ dayjs(line.datetime + '+00:00').format('YYYY/MM/DD HH:mm:ss') }}</td>
-                <td class="tdStyle">{{ line.method }}</td>
-                <td class="tdStyle">{{ line.url }}</td>
-                <td class="tdStyle">{{ line.ip }}</td>
-                <td class="tdStyle">{{ line.status }}</td>
-            </tr>
-        </table>
-    </div>
-    <div class="flex justify-center">
-        <div class="justify-center">
-            <h4 class="mt-3 font-bold text-lg">Statistics:<span class="ml-2 italic text-sm font-semibold">(updated hourly)</span></h4>
-            <p class="font-normal text-base"><i>category_count:</i> <span class="ml-0.5 font-semibold">{{ statistics?.category_count }}</span></p>
-            <p class="font-normal text-base"><i>book_count:</i> <span class="ml-0.5 font-semibold">{{ statistics?.book_count }}</span></p>
-            <p class="font-normal text-base"><i>exemplar_count:</i> <span class="ml-0.5 font-semibold">{{ statistics?.exemplar_count }}</span></p>
-            <p class="font-normal text-base"><i>database_size:</i> <span class="ml-0.5 font-semibold">{{ statistics?.mysql_count }}</span></p>
+    <div class="font-oldstandardtt text-amber-950 text-lg">
+        <div class="flex justify-center">
+            <h1 class="mt-2 mb-2 font-bold text-4xl">:<span class="ml-1">:</span> library_api <span class="mr-1">:</span>:</h1>
         </div>
+        <div class="flex justify-center">
+            <h3 class="mb-4 font-medium text-2xl">Accumulated: <i class="mr-0.5">F</i>{{ accumulatedMoney }}</h3>
+        </div>
+        <div class="flex justify-center">
+            <table class="bg-stone-50 border border-stone-500">
+                <tr>
+                    <th class="p-1 bg-stone-300 font-semibold border-b border-stone-500 whitespace-nowrap">When ?</th>
+                    <th class="thStyle">Method</th>
+                    <th class="thStyle">URL</th>
+                    <th class="thStyle">IP</th>
+                    <th class="thStyle">Status</th>
+                </tr>
+                <tr v-if="lines?.length === 0">
+                    <td colspan="5" class="p-1 font-medium italic whitespace-nowrap">No Records Found!</td>
+                </tr>
+                <tr v-for="line in lines" :key="line.id" :class="{ 'bg-stone-200': line.id % 6 > 2 }">
+                    <td class="px-1 whitespace-nowrap">{{ dayjs(line.datetime + '+00:00').format('YYYY/MM/DD HH:mm:ss') }}</td>
+                    <td class="tdStyle">{{ line.method }}</td>
+                    <td class="tdStyle">{{ line.url }}</td>
+                    <td class="tdStyle">{{ line.ip }}</td>
+                    <td class="tdStyle">{{ line.status }}</td>
+                </tr>
+            </table>
+        </div>
+        <div class="flex justify-center">
+            <div class="justify-center">
+                <h4 class="mt-3 font-bold text-xl">Statistics:<span class="ml-2 italic text-sm font-semibold">(updated hourly)</span></h4>
+                <p class="font-normal"><i>category_count:</i> <span class="ml-0.5 font-semibold">{{ statistics?.category_count }}</span></p>
+                <p class="font-normal"><i>book_count:</i> <span class="ml-0.5 font-semibold">{{ statistics?.book_count }}</span></p>
+                <p class="font-normal"><i>exemplar_count:</i> <span class="ml-0.5 font-semibold">{{ statistics?.exemplar_count }}</span></p>
+                <p class="font-normal"><i>database_size:</i> <span class="ml-0.5 font-semibold">{{ statistics?.mysql_count }}</span></p>
+            </div>
+        </div>
+        <div class="h-20" />
     </div>
-    <div class="h-20" />
 </template>
 <style lang="postcss" scoped>
 .tdStyle {
-    @apply px-1 border-l border-zinc-500;
+    @apply px-1 border-l border-stone-500;
 }
 .thStyle {
-    @apply p-1 bg-zinc-300 font-semibold border-l border-b border-zinc-500;
+    @apply p-1 bg-stone-300 font-semibold border-l border-b border-stone-500;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                oldstandardtt: ['Old Standard TT'],
             },
         },
     },
