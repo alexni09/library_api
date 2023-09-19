5fd934cf3ded8c1bb8cbf61e66f5bdf7667d9a99
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-19 15:42:04 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\DB;
-
-Route::get('/db', function() {
-    $r = DB::table('information_schema.tables')->selectRaw('sum(data_length) + sum(index_length) as total_sum')->first()->total_sum;
-    dd($r);
-});
 
 Route::get('/', function () {
     return Inertia::render('Index');
