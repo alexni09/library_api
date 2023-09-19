ef36deb2d1ca22442ea66049a71cb5a88e944b14
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-19 15:27:25 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/db', function() {
+    $r = DB::table('information_schema.tables')->where('TABLE_NAME','exemplars')->first()->TABLE_ROWS;
+    dd($r);
+});
 
 Route::get('/', function () {
     return Inertia::render('Index');
