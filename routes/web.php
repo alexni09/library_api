b5476d9cca8ed012ec5da2f0207bdb22e9c7f6f4
alexni09 <137412803+alexni09@users.noreply.github.com>
2023-09-19 15:36:17 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
 
 Route::get('/db', function() {
-    $r = DB::table('information_schema.tables')->where('TABLE_NAME','exemplars')->first()->TABLE_ROWS;
+    $r = DB::table('information_schema.tables')->selectRaw('sum(data_length) + sum(index_length) as total_sum')->first()->total_sum;
     dd($r);
 });
 
